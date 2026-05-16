a26fcd3a5e2acedcc8122399ba165f5ec002aebb
Lesego303 <lesegolethoko@gmail.com>
2026-05-16 22:11:45 +0200

    Add files via upload

---
+++ garden_advice.py
@@ -0,0 +1,20 @@
+# Gardening advice app
+
+# TODO:
+# Create functions to avoid repeating code
+# Add more comments/documentation
+# Replace hardcoded month names with variables
+
+month = "May"
+
+if month == "December":
+    print("Water plants regularly because it is summer.")
+
+elif month == "June":
+    print("Protect plants from frost during winter.")
+
+elif month == "September":
+    print("Spring is perfect for planting flowers.")
+
+else:
+    print("Check seasonal gardening tips.")
